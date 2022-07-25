06b16e5fb25dbe9231555c496b07cc7a30994210
girish007itcodehelp <girish.itcodehelp@gmail.com>
2022-07-25 09:03:28 +0530

    changed

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,7 +14,6 @@ const Layout: React.FC<Props> = ({ children }) => {
       {/* <CehckoutSlider /> */}
       <Header />
       <CartIcon />
-
       {children}
       {/* <Footer /> */}
     </div>
